715561c82938f1651adc6498e3a8dfcedb994f6a
Tapas Halder <Krishnanduzx10rh2@gmail.com>
2026-09-24 15:29:46 +0530

    Update TRAFFIC_API_URL to new environment variable

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-TRAFFIC_API_URL = os.getenv("TRAFFIC_API_URL")
+TRAFFIC_API_URL = os.getenv("NB0SZBoWrJNALAGVMgK7ESo09TGNFvdV")
 
 TRAFFIC_API_KEY = os.getenv("TRAFFIC_API_KEY")
