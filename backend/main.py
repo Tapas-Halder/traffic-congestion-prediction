0ae6c052afd04847d1208c5e05bda0b7ed019c31
Tapas Halder <Krishnanduzx10rh2@gmail.com>
2026-09-24 17:49:26 +0530

    Simplify project without database: update backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from config import APP_NAME,FRONTEND_URL
-from database import Base,engine
 from routers.traffic import router
-Base.metadata.create_all(bind=engine)
 app=FastAPI(title=APP_NAME,version="1.0.0")
 app.add_middleware(CORSMiddleware,allow_origins=["*"] if FRONTEND_URL=="*" else [x.strip() for x in FRONTEND_URL.split(",")],allow_credentials=False,allow_methods=["*"],allow_headers=["*"])
 app.include_router(router)
